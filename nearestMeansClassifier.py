47fca0ed085527174016ac8051430cdda54b3411
Tejas <tejas8acharya@gmail.com>
2023-06-15 07:31:03 +0530

    Add imports to nearestMeansClassifier.py

---
+++ nearestMeansClassifier.py
@@ -0,0 +1,10 @@
+# Name: Tejas Acharya
+# Class: EE-559 
+# Date: 15-06-2023
+# Assignment: Homework 2
+
+import numpy as np
+from scipy.spatial.distance import cdist
+from plotDecBoundaries import plotDecBoundaries
+
+
